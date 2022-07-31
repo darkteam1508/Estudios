e288c18c0280eece459b791f2ad9fab782ffa2ea
Eduardo Sanchez Flores <eduardosflores09@gmail.com>
2022-07-31 12:58:18 -0500

    se va agregar la actividad 1 del bloque 2 de mis estudios en c++

---
+++ actividades del 2do bloque en c++/actividad1Bloque2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+float a,b, resultado = 0;
+
+cout<<"digite el valor de a: "; cin>>a;
+cout<<"digite el valor de b: "; cin>>b;
+
+resultado = (a)*(b);
+
+cout<<"\nEl resultado es: "<<resultado<<endl;
+
+return 0;
+}
