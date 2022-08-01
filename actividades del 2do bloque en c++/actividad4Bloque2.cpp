ca030ad1ab0e0e0138075c3852e32818e4085107
Eduardo Sanchez Flores <eduardosflores09@gmail.com>
2022-08-01 14:19:58 -0500

    se va agregar la actividad 4 del bloque 2 de mis estudios en c++

---
+++ actividades del 2do bloque en c++/actividad4Bloque2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+float a,b,c,d, resultado = 0;
+
+cout<<"digite el valor de a: "; cin>>a;
+cout<<"digite el valor de b: "; cin>>b;
+cout<<"digite el valor de c: "; cin>>c;
+cout<<"ditite el valor de d: "; cin>>d;
+
+resultado = (a)+(b/(c-d));
+
+cout<<"el resultado es: "<<resultado<<endl;
+    return 0;
+}
