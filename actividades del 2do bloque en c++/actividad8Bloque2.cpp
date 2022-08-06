4b88ccfec62ade83fa1be865ce063aae92b0872d
Eduardo Sanchez Flores <eduardosflores09@gmail.com>
2022-08-05 21:21:20 -0500

    se va agregar la actividad 8 del bloque 2 de mis estudios en c++

---
+++ actividades del 2do bloque en c++/actividad8Bloque2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+	float cateto1,cateto2,hipotenusa;
+	
+	cout<<"digite el cateto 1: ";
+	cin>>cateto1;
+	cout<<"digite el cateto 2: "; 
+	cin>>cateto2;
+	
+	hipotenusa = sqrt(pow(cateto1,2)+pow(cateto2,2));
+	
+	cout.precision(2); 
+	
+	cout<<"\nLa hipotenusa del triangulo es: "<<hipotenusa<<endl;
+	
+	
+	return 0;
+}
