f2bfd5d96a2c10b6fb62279f411f28e446c1da9d
Eduardo Sanchez Flores <eduardosflores09@gmail.com>
2022-08-15 14:38:41 -0500

    se va agregar el ejercicio 2 de la sentencia if de mis estudios en c++

---
+++ ejercicio 2 sentencia if.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float promedio;
+	
+	cout<<"digite el promedio: "; cin>>promedio;
+
+	if(promedio >= 7)
+	{
+		cout<<"Aprobado";
+	}
+	else
+	{
+		cout<<"Reprobado";
+	}
+	
+	
+	return 0;
+}
